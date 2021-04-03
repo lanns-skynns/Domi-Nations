2464ff3691f8d65533a1122b970c665fcc51e154
lanns-skynns <56886235+lanns-skynns@users.noreply.github.com>
2021-04-03 22:17:36 +0200

    maj classe pile

--- src/dominations/Main.java
+++ src/dominations/Main.java
@@ -2,21 +2,41 @@ package dominations;
 
 import dominations.model.*;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Pile pile = new Pile(36);
+        Pile pile = new Pile(36,4);
 
-        Carte[] cartesDansPile = pile.getCartesPile();
-        System.out.println(cartesDansPile[20].getInfoCarte());
+        List <Carte> cartesDansPile = pile.getCartesPile();
+       // System.out.println(cartesDansPile.get(0).getInfoCarte());
         int i;
 
-        //System.out.print("Les dominos en jeu sont les suivants  : ");
-
-
-        for (i=0;i<cartesDansPile.length;i++){
-            System.out.println(cartesDansPile[i].getInfoCarte());
+        for (i=0;i<cartesDansPile.size();i++){
+            System.out.println(cartesDansPile.get(i).getInfoCarte());
         }
+        System.out.println(" ");
+        System.out.println("Cartes restantes -> "+pile.getNombreCartesRestantes());
+
+        List <Carte> cartesPiochees= pile.piocherCartes();
+        int j;
+        System.out.println("cartes piochées : ");
+        for (j=0;j<cartesPiochees.size();j++){
+            System.out.println(cartesPiochees.get(j).getInfoCarte());
+        }
+
+        System.out.println("Carte restantes -> "+pile.getNombreCartesRestantes());
+
+        List <Carte> cartesPiopio= pile.piocherCartes();
+
+        System.out.println("cartes piochées : ");
+        for (j=0;j<cartesPiochees.size();j++){
+            System.out.println(cartesPiopio.get(j).getInfoCarte());
+        }
+
+        System.out.println("Carte restantes -> "+pile.getNombreCartesRestantes());
+
     }
 }
--- src/dominations/model/Pile.java
+++ src/dominations/model/Pile.java
@@ -1,71 +1,79 @@
 package dominations.model;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Pile {
 
-    Map map = new HashMap();
-
-    private int nbrCarte;
-    private Carte[] cartesPile;
+    private final int nbrCarte;
+    private List<Carte> cartesPile;
     private int nbrCartesRestantes;
+    private final int nbrCartesApiocher;
 
-    public Pile(int nbrCartes){
+    /* ************
+    Constructeur de la classe
+    ************ */
+
+    public Pile(int nbrCartes,int nbrCartesApiocher) {
         this.nbrCarte = nbrCartes;
-        cartesPile = creerPile(this.nbrCarte);
-    };
-
-    //public  Carte[] enleverCarte(){ }
-
-    public  void melanger(){
-
+        this.cartesPile = creerPile(this.nbrCarte);
+        this.nbrCartesRestantes = cartesPile.size();
+        this.nbrCartesApiocher=nbrCartesApiocher;
     }
 
-    private Carte[] creerPile(int nbrDeCarte){
-        int i;
-        int carteAleatoire;
-        int[] indiceDejaChoisi = new int[nbrDeCarte];
-        Carte[] pourLapioche=new Carte[nbrDeCarte];
-        for (i=0;i<nbrDeCarte;i++){
-            carteAleatoire=(int)(Math.round(Math.random()*47));
-
-            while(contains(indiceDejaChoisi,Integer.toString(carteAleatoire))){
-                carteAleatoire=(int)(Math.round(Math.random()*47));
-            }
-
-            indiceDejaChoisi[i]=carteAleatoire;
-            int[] nbrCouronne={this.couronne[carteAleatoire][0],this.couronne[carteAleatoire][1]};
-            char [] typePaysage={this.type[carteAleatoire][0],this.type[carteAleatoire][1]};
-            Carte intermediaire=new Carte(this.numCarte[carteAleatoire],nbrCouronne,typePaysage);
-            pourLapioche[i]=intermediaire;
-        }
-        return pourLapioche;
-    }
-
-    public Carte[] getCartesPile(){
+    public List<Carte> getCartesPile() {
         return cartesPile;
     }
 
-    private boolean contains(int[] T, String val) {
-        return Arrays.toString(T).contains(val);
+    public int getNombreCartesRestantes() {
+        return nbrCartesRestantes;
     }
 
+    /* ***************
+    La méthode piocherCartes permet  de piocher un certain nombre de carte à chaque de jeu
+    *************** */
 
-
-    private  int[] numCarte(){
+    public List<Carte> piocherCartes() {
+        List<Carte> cartesPiochees = new ArrayList<>();// cette liste de carte contiendra les cartes qui seront piochées
         int i;
-        int[] numC= new int[48];
-        for(i=0;i<48;i++){
-            numC[i]=i;
+        for (i = 0; i < nbrCartesApiocher; i++) {// cette boucle permet de piocher un certain de nombre de carte à chaque tour  piocher à chaque tour;
+            cartesPiochees.add(this.cartesPile.get(0));// on pioche la prémière carte de la pile
+            this.cartesPile.remove(0);// on supprime chaque carte déja piochée
         }
-        return numC;
+        this.nbrCartesRestantes = cartesPile.size();
+        return cartesPiochees;
     }
 
-    private int[] numCarte=numCarte();
+    public void melanger() {
 
-    private int[][] couronne = {
+    }
+
+
+
+    /* ************
+        Cette méthode permet de choisir un certain nombre de carte de manière alétoire pour la pile de carte.
+    ************* */
+
+
+    private List<Carte> creerPile(int nbrDeCarte) {
+
+        int i;
+        int carteAleatoire;
+        List<Carte> pourLapioche = new ArrayList<>();// cette veriable contiendra la liste de carte qui servira de pioche
+        for (i = 0; i < nbrDeCarte; i++) {// cette boucle permet de sélectionner un nombre précis de carte pour la pile
+            carteAleatoire = (int) (Math.round(Math.random() * (numCartes.size() - 1)));// on sélectionne  un nombre aléatoire entre 0 et et le nombre de carte qui n'ont pas encore été ajouté dans le pile
+            int[] nbrCouronne = {this.couronnes.get(carteAleatoire)[0], this.couronnes.get(carteAleatoire)[1]};
+            char[] typePaysage = {this.paysages.get(carteAleatoire)[0], this.paysages.get(carteAleatoire)[1]};
+            Carte intermediaire = new Carte(this.numCartes.get(carteAleatoire), nbrCouronne, typePaysage);// création d'une carte
+            pourLapioche.add(intermediaire);// ajout de  ²                                                                                             cette carte dans la pioche
+            this.numCartes.remove(carteAleatoire);//suppression...
+            this.couronnes.remove(carteAleatoire);//de la carte
+            this.paysages.remove(carteAleatoire);// choisi
+        }
+        return pourLapioche; // retourne la pioche
+    }
+
+
+    final private int[][] LISTECOURONNES = {
             {0, 0},
             {0, 0},
             {0, 0},
@@ -115,7 +123,10 @@ public class Pile {
             {0, 2},
             {0, 3},
     };
-    private  char[][] type = {
+
+
+
+   final private char[][] LISTEPAYSAGES = {
             {'c', 'c'},
             {'c', 'c'},
             {'f', 'f'},
@@ -166,13 +177,36 @@ public class Pile {
             {'c', 'n'},
     };
 
-    public int getNombreCartesRestantes(){
-        return nbrCartesRestantes;
+    private List<Integer> numCartes() {// créer une "List" qui contient les numéros de cartes au lieu du tableau de base.
+        int i;
+        List<Integer> numC;
+        numC = new ArrayList<>();
+        for (i = 0; i < 48; i++) {
+            numC.add(i);
+        }
+        return numC;
     }
 
+    private List<int[]> couronnes() { //créer une "List" qui contient les coruonnes au lieu du tableau de base.
+        int j;
+        List<int[]> c = new ArrayList<>();
+        for (j = 0; j < 48; j++) {
+            c.add(LISTECOURONNES[j]);
+        }
+        return c;
+    }
 
-
-
+    private List<char[]> paysages() {// créer une "List" qui paysages de cartes au lieu du tableau de base.
+        int j;
+        List<char[]> c = new ArrayList<>();
+        for (j = 0; j < 48; j++) {
+            c.add(LISTEPAYSAGES[j]);
+        }
+        return c;
+    }
+     private List<Integer> numCartes = numCartes();
+     private List<char[]> paysages = paysages();
+     private List<int[]> couronnes = couronnes();
 
 }
 
